875dca682a1bbfb1120c9de96753fdaac86c4161
EC2 Default User <ec2-user@ip-172-31-62-23.ec2.internal>
2021-02-01 11:13:03 +0000

    ouyou2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+
+  before_action :baria_user, only: [:edit]
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def baria_user
+       book = Book.find(params[:id])
+    if book.user != current_user
+       redirect_to books_path
+    end
+  end
+
+end
